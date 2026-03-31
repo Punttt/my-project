bc0635256ff073f29bdd83fc3a49165696a4947e
Pontus Johansson <johanssonp998@gmail.com>
2026-03-31 10:59:29 +0200

    hämtar värden från formulär och lägger in i en nytt objekt

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
+// Interface
+interface CourseInfo {
+    name: string;
+    code: string;
+    progression: 'A' | 'B' | 'C';
+    url: string;
+}
+
+// Skapar en tom array
+let courses : CourseInfo[] = [];
+
 // Inväntar DOM
 document.addEventListener("DOMContentLoaded", () => {
     
@@ -13,6 +24,21 @@ document.addEventListener("DOMContentLoaded", () => {
 // Funktioner 
 function handleSubmit(e: SubmitEvent): void {
     e.preventDefault();
-
     console.log("Formuläret skickades!");
+
+    // Hämtar de nya värdena som är inmatade i formuläret
+    const nameInput = (document.querySelector("#name") as HTMLInputElement).value;
+    const codeInput = (document.querySelector("#code") as HTMLInputElement).value;
+    const progressionInput = (document.querySelector("#progression") as HTMLInputElement).value;
+    const syllabusInput = (document.querySelector("#syllabus") as HTMLInputElement).value;
+
+
+    const course : CourseInfo = {
+        name:nameInput,
+        code:codeInput,
+        progression:progressionInput,
+        url:syllabusInput
+    }
+
+    console.log(course);
 }
